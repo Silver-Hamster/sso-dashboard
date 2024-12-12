923cb248d8b3022a810b9b5f3a7b97682979ad80
Amit27saini <amitsaini272001@gmail.com>
2024-12-12 11:28:09 +0530

    baseurl updated

--- src/plugins/axios.ts
+++ src/plugins/axios.ts
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const axiosInstance = axios.create({
     // baseURL: 'http://localhost:3002', // Replace with your API base URL
-    baseURL: 'https://api.socialsecurityofficeus.com/api', // Replace with your API base URL
+    baseURL: 'https://api.socialsecurityofficeus.com', // Replace with your API base URL
 
     headers: {
         'Content-Type': 'application/json',
